6050952bf6da57673c0eb23916179a3e2bd2d004
Riccardo Petricca <3359943+rc2pc2@users.noreply.github.com>
2024-10-01 10:24:10 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+      return {
+          
+      }
+    },
+    components:{
+      
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss";
+
+</style>
